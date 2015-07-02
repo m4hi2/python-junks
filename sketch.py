05aa0e350ce5cde35e132e0ef40bb848e75ea76a
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-03 02:09:49 +0600

    Added support to create and write into a file

--- sketch.py
+++ sketch.py
@@ -8,15 +8,26 @@ try:
   data = open("sketch.txt")
   for line in data:
     try:
-      (role, line_spoken) = data.split(':', 1)
-      line_spoken = line_spoken.stip()
+      (role, line_spoken) = line.split(':', 1)
+      line_spoken = line_spoken.strip()
       if role == "Man":
         man.append(line_spoken)
       elif role == "Other Man":
         other.append(line_spoken)
     except ValueError:
       pass
-    data.close()
+  print(man)
+  print(other)
+  try:
+    output = open("out.txt", "w")
+    print(man, file= output)
+    print(other, file = output)
+    output.close()
+    print("\n", "Job done")
+    
+  except IOError:
+    print("You fucker, where is my file in the system?")
+    
 except IOError:
   print("Fuck, You forgot to put the file in")
     
