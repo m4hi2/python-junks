7558f7bb009897baafafd3aa52eacfe088e999c7
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-05 23:55:59 +0600

    added support for managing files with 'with'

--- sketch.py
+++ sketch.py
@@ -2,47 +2,27 @@
 This file has evolved a lot, and now this is going to be my I/O play gorund;
 -----------------------------------------------------------------------------'''
 man = []
-other = []
+other_man = []
 
 try:
-  data = open("sketch.txt")
-  for line in data:
-    try:
-      (role, line_spoken) = line.split(':', 1)
-      line_spoken = line_spoken.strip()
-      if role == "Man":
-        man.append(line_spoken)
-      elif role == "Other Man":
-        other.append(line_spoken)
-    except ValueError:
-      pass
-  print(man)
-  print(other)
+  with open("sketch.txt") as data:
+    for line in data:
+      try:
+        (role, line_spoken) = line.split(":", 1)
+        if role == "Man":
+          man.append(line_spoken)
+        elif role == "Other Man":
+          other_man.append(line_spoken)
+      except:
+        pass
   try:
-    man_data = open("man_data.txt", "w")
-    other_data = open("other_man_data.txt", "w")
-    print(man, file= man_data)
-    print(other, file = other_data)
-    man_data.close()
-    other_data.close()
-    print("\n", "Job done")
-    
-  except IOError:
-    print("You fucker, where is my file in the system?")
+    with open("man_data.txt", 'w') as man_data:
+      print(man, file = man_data)
+    with open("other_man_data.txt", 'w') as other_data:
+      print(other_man, file = other_data)
+  except IOError as irr:
+    print("Where the fuck is the output file?" + str(irr))
 
-  finally:
-    man_data.close()
-    other_data.close()
-    
-    
-except IOError:
-  print("Fuck, You forgot to put the file in")
-  
-
-finally:
-  data.close()
-    
-      
-      
-      
+except IOError as irr:
+  print("Did you put in any input file?" + str(irr))
       
