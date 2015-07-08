3a6e1ac2af93cfac3a55d21d57b543c9d6c6b96b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-08 15:16:01 +0600

    added support for printing in file

--- modules/nester/build/lib/nester.py
+++ modules/nester/build/lib/nester.py
@@ -3,12 +3,13 @@ This is the most idiotic module ever written -_- fuckers made me do it;
 bal chal;
 
 """
-def print_lol (the_list,indent= False,  tab_width=0):
+import sys
+def print_lol (the_list,indent= False,  tab_width=0,of=sys.stdout ):
   for each_item in the_list:
     if isinstance (each_item, list):
-      print_lol (each_item,indent, tab_width+1)
+      print_lol (each_item,indent, tab_width+1, of)
     else:
       if indent:
           for each in range(tab_width):
-            print ("\t ", end="")
-      print (each_item)
+            print ("\t ", end="" ,file = of)
+      print (each_item, file= of)
