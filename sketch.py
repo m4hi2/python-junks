2eb0340393c4cee74a71a83591b24943412cbecb
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-08 20:42:19 +0600

    reduced code

--- sketch.py
+++ sketch.py
@@ -5,26 +5,21 @@ import nester
 
 man = []
 other_man = []
-
 try:
-  with open("sketch.txt") as data:
+  with open("sketch.txt") as data, open("man_data.txt", "w") as man_data, open("other_man_data.txt", "w") as other_data:
     for line in data:
       try:
         (role, line_spoken) = line.split(":", 1)
         if role == "Man":
           man.append(line_spoken)
+          
         elif role == "Other Man":
           other_man.append(line_spoken)
+          
       except:
         pass
-  try:
-    with open("man_data.txt", 'w') as man_data:
-      nester.print_lol(man,  of= man_data)
-    with open("other_man_data.txt", 'w') as other_data:
-      nester.print_lol(other_man, of = other_data)
-  except IOError as irr:
-    print("Where the fuck is the output file?" + str(irr))
+    nester.print_lol(man, of= man_data,   e= "")
+    nester.print_lol(other_man, of = other_data, e= "")
 
-except IOError as irr:
-  print("Did you put in any input file?" + str(irr))
-      
+except IOError as err:
+  print('File error: ' + str(err)) 
