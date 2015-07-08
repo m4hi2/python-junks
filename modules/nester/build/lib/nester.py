74c03cec63f36780cca419726bb207cfd7bf94f9
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-08 20:14:08 +0600

    Added support to use 'end=' with print_lol()

--- modules/nester/build/lib/nester.py
+++ modules/nester/build/lib/nester.py
@@ -4,12 +4,12 @@ bal chal;
 
 """
 import sys
-def print_lol (the_list,indent= False,  tab_width=0,of=sys.stdout ):
+def print_lol (the_list,indent= False,  tab_width=0,of=sys.stdout, e = "" ):
   for each_item in the_list:
     if isinstance (each_item, list):
-      print_lol (each_item,indent, tab_width+1, of)
+      print_lol (each_item,indent, tab_width+1, of, e)
     else:
       if indent:
           for each in range(tab_width):
-            print ("\t ", end="" ,file = of)
-      print (each_item, file= of)
+            print ("\t ", end=e ,file = of)
+      print (each_item, end = e, file= of)
