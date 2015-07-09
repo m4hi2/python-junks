b32a82b57b3e9c7b0d43d412efcd4e3142cbe37e
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-09 15:49:56 +0600

    Now everything works with pickle for the first time ;)

--- sketch.py
+++ sketch.py
@@ -1,12 +1,12 @@
 '''-----------------------------------------------------------------------------
 This file has evolved a lot, and now this is going to be my I/O play gorund;
 -----------------------------------------------------------------------------'''
-import nester
+import pickle
 
 man = []
 other_man = []
 try:
-  with open("sketch.txt") as data, open("man_data.txt", "w") as man_data, open("other_man_data.txt", "w") as other_data:
+  with open("sketch.txt") as data, open("man_data.txt", "wb") as man_data, open("other_man_data.txt", "wb") as other_data:
     for line in data:
       try:
         (role, line_spoken) = line.split(":", 1)
@@ -18,8 +18,8 @@ try:
           
       except:
         pass
-    nester.print_lol(man, of= man_data,   e= "")
-    nester.print_lol(other_man, of = other_data, e= "")
+    pickle.dump(man,man_data)
+    pickle.dump(other_man,other_data)
 
 except IOError as err:
   print('File error: ' + str(err)) 
