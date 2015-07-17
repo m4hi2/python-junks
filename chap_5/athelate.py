4bad26e1be32be644b10d198cf4e7423e7b648a2
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-17 20:44:02 +0600

    Updated get_data() func to sanitize and sort out fastest times

--- chap_5/athelate.py
+++ chap_5/athelate.py
@@ -17,17 +17,10 @@ def sanitize(time_string):
 def get_data(filename):
 	try:
 		with open(filename) as file:
-			# return file.readline().strip().split(",")
 			file_data = file.readline().strip().split(",")
-			return {'Name' : file_data.pop(0), 'DoB' : file_data.pop(0), 'Times': file_data}
+			return {'Name' : file_data.pop(0), 'DoB' : file_data.pop(0), 'Times': str(sorted(sanitize(t) for t in file_data)[0:3])}
 	except IOError as IOerr:
 		print("Where's ma file bae?" +  str(IOerr))
 		return None
-
 sarah = get_data("sarah2.txt")
-#Let's use a dictionary object to hold data for sarah
-# sarah_structured = dict() #a curly brace would have done the trick -_-
-# sarah_structured ["Name"] = sarah.pop(0)
-# sarah_structured ["DoB"] = sarah.pop(0)
-# sarah_structured ["Times"] = sarah
-print(sarah["Name"] +"'s fastest times are:" +str(sorted(sanitize(t) for t in sarah["Times"])[0:3]))
+print(sarah["Name"] +"'s fastest times are:" + sarah["Times"])
