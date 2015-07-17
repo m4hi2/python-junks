5569b0f5b118d57fdad6b6eeb539bda3b64de038
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-17 20:25:52 +0600

    Updated get_data() func to directly create a dictionary for the format

--- chap_5/athelate.py
+++ chap_5/athelate.py
@@ -3,10 +3,6 @@ The coach needs the fastest racer but he's an asshole. He saved data like an idi
 So, I need to stick my ass to the chair for a bit longer and format down his mess.
 It's really messy, did any one say messi? :D
 '''
-james = []
-julie = []
-mikey = []
-sarah = []
 #takes tring input then converts it to a '.' seperator if the seperator is either ":" or '-'
 def sanitize(time_string): 
 	if '-' in time_string:
@@ -21,15 +17,17 @@ def sanitize(time_string):
 def get_data(filename):
 	try:
 		with open(filename) as file:
-			return file.readline().strip().split(",")
+			# return file.readline().strip().split(",")
+			file_data = file.readline().strip().split(",")
+			return {'Name' : file_data.pop(0), 'DoB' : file_data.pop(0), 'Times': file_data}
 	except IOError as IOerr:
 		print("Where's ma file bae?" +  str(IOerr))
 		return None
 
 sarah = get_data("sarah2.txt")
 #Let's use a dictionary object to hold data for sarah
-sarah_structured = dict() #a curly brace would have done the trick -_-
-sarah_structured ["Name"] = sarah.pop(0)
-sarah_structured ["DoB"] = sarah.pop(0)
-sarah_structured ["Times"] = sarah
-print(sarah_structured["Name"] +"'s fastest times are:" +str(sorted(sanitize(t) for t in sarah_structured["Times"])[0:3]))
+# sarah_structured = dict() #a curly brace would have done the trick -_-
+# sarah_structured ["Name"] = sarah.pop(0)
+# sarah_structured ["DoB"] = sarah.pop(0)
+# sarah_structured ["Times"] = sarah
+print(sarah["Name"] +"'s fastest times are:" +str(sorted(sanitize(t) for t in sarah["Times"])[0:3]))
