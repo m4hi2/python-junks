38aec2b18021ef5cadd895f568e5f2194c922f7f
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-07-17 18:49:39 +0600

    File back from master

--- chap_5/athelate.py
+++ chap_5/athelate.py
@@ -3,10 +3,10 @@ The coach needs the fastest racer but he's an asshole. He saved data like an idi
 So, I need to stick my ass to the chair for a bit longer and format down his mess.
 It's really messy, did any one say messi? :D
 '''
-james = [ ]
-julie = [ ] 
-mikey = [ ]
-sarah = [ ]
+james = []
+julie = [] 
+mikey = []
+sarah = []
 def sanitize(time_string): #takes tring input then converts it to a '.' seperator if the seperator is either ":" or '-'
 	if '-' in time_string:
 		splitter = '-'
@@ -30,7 +30,7 @@ julie = get_data("julie.txt")
 mikey = get_data("mikey.txt")
 sarah = get_data("sarah.txt")
 
-print("james:",sorted(set([sanitize(t) for t in james]))[0:3])
+print("james:",sorted(set(sanitize(t) for t in james))[0:3])
 print("julie:",sorted(set(sanitize(t) for t in julie))[0:3])
 print("mikey:",sorted(set(sanitize(t) for t in mikey))[0:3])
-print("sarah:",sorted(set([sanitize(t) for t in sarah]))[0:3])
+print("sarah:",sorted(set(sanitize(t) for t in sarah))[0:3])
