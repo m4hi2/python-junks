7d613d9e957b041f8673b8f62870da16fd096646
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2015-08-06 01:41:24 +0600

    Added the function to devise a number by 1-10 each time

--- euler/5_euler.py
+++ euler/5_euler.py
@@ -1,3 +1,17 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+# This function actually checks if the number is divisible by all numbers form "1-10" (will elevate that shortly)
+def is_devisible(number):
+	flag = 0 # default for not devised
+	for i in range(1, 11):
+		if number % i == 0 :
+			flag = 1
+		else:
+			flag = 0
+			break
+	if flag == 0:
+		return False
+	else:
+		return True
+
